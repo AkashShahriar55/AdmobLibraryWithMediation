57ad261b1a5408dfd89e586c5bea852a46560750
akash <akashshahriar.kgs@gmail.com>
2022-06-02 13:24:00 +0600

    initial setup

---
+++ app/src/main/java/com/cookietech/admoblibrarywithmediation/TestApplication.kt
@@ -0,0 +1,12 @@
+package com.cookietech.admoblibrarywithmediation
+
+import android.app.Application
+
+class TestApplication: Application() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
