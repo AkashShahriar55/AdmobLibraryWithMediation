7515881fcb7edc7132bd21c717d6a60e0048e5c1
akash <akashshahriar.kgs@gmail.com>
2022-06-02 13:32:16 +0600

    metadata added

---
+++ app/src/main/java/com/cookietech/admoblibrarywithmediation/nativead/NativeAdActivity.kt
@@ -0,0 +1,12 @@
+package com.cookietech.admoblibrarywithmediation.nativead
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cookietech.admoblibrarywithmediation.R
+
+class NativeAdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_native_ad)
+    }
+}
