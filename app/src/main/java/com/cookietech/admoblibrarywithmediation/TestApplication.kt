131fc371d785546a79824b2400872296e2b76159
akash <akashshahriar.kgs@gmail.com>
2022-06-02 13:41:22 +0600

    Mobile ads initialized

--- app/src/main/java/com/cookietech/admoblibrarywithmediation/TestApplication.kt
+++ app/src/main/java/com/cookietech/admoblibrarywithmediation/TestApplication.kt
@@ -1,12 +1,19 @@
 package com.cookietech.admoblibrarywithmediation
 
 import android.app.Application
+import android.util.Log
+import com.google.android.gms.ads.MobileAds
 
 class TestApplication: Application() {
 
 
     override fun onCreate() {
         super.onCreate()
+
+
+        MobileAds.initialize(this) {
+            Log.d("ads_initialize", "onCreate: " + it.adapterStatusMap)
+        }
     }
 
 }
--- app/src/main/java/com/cookietech/admoblibrarywithmediation/nativead/NativeAdActivity.kt
+++ app/src/main/java/com/cookietech/admoblibrarywithmediation/nativead/NativeAdActivity.kt
@@ -8,5 +8,7 @@ class NativeAdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_native_ad)
+
+
     }
 }
